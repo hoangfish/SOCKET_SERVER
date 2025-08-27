d4d5b76c5eccc5140f8d58d37f983077a370b9fb
fish <phphuc2417@clc.fitus.edu.vn>
2025-08-27 11:19:49 +0700

    update room status

--- server.js
+++ server.js
@@ -6,23 +6,21 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 
-   const token = socket.handshake.query.token;
-    console.log("Token from client:", token);
+  const token = socket.handshake.query.token;
+  console.log("Token from client:", token);
 
-    if (token !== "123") {
-        console.log("Invalid token, disconnecting");
-        socket.disconnect(true);
-        return;
-    }
+  if (token !== "123") {
+    console.log("Invalid token, disconnecting");
+    socket.disconnect(true);
+    return;
+  }
 
-
-  socket.on("room:status", (roomId) => {
-    console.log("Room was booked:",roomId);
-    io.emit("room:status:changed", roomId);
+  socket.on("room:status", (data) => {
+    console.log("Room status changed:", data);
+    io.emit("room:status:changed", data);
   });
 
   socket.on("disconnect", () => {
